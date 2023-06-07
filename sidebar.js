55c4d90c5c3c36dc43cbfb52992f381bc52dd4ea
yourdevilfriends <heckmaster@protonmail.com>
2023-06-07 18:13:39 +0100

    Art...!

--- sidebar.js
+++ sidebar.js
@@ -3,12 +3,12 @@ var html = '<a href="/"><button style="background-image: url(/images/backgrounds
 html +='<button style="background-image: url(/images/backgrounds/buttons/personal.png);">Personal</button></summary><ul><li><a href="/about.html">About</a></li><li><a href="/blog.html">Inane rambling</a></li></ul>';
 
 html +=
-    '<button style="background-image: url(/images/backgrounds/buttons/artwork.png);">Artwork</button></summary><ul><li><a href="/gallery.html">Gallery</a></li><li><a href="/comix.html">Comics</a></li></ul>';
+    '<button style="background-image: url(/images/backgrounds/buttons/artwork.png);">Artwork</button></summary><ul><li><a href="/gallery.html">Art gallery</a></li><li><a href="/comix.html">Comics</a></li></ul>';
 
 html +=
     '<button style="background-image: url(/images/backgrounds/buttons/foryou.png);">For you</button><ul><li><a href="/accessibility.html">Web accessibility guide</a></li><li><a href="/freebies.html">Free graphics</a></li><li><a href="/scans.html">Scans</a></li></ul>';
 
 html +=
-    '<button style="background-image: url(/images/backgrounds/buttons/etcetera.png);">Et cetera</button></summary><ul><li><a href="https://dungbeetle.neocities.org/">Linkroll</a></li><li><a href="/updates.html">Update log</a></li></ul>';
+    '<button style="background-image: url(/images/backgrounds/buttons/etcetera.png);">Et cetera</button></summary><ul><li><a href="https://dungbeetle.neocities.org/" target="_blank">Linkroll</a></li><li><a href="/updates.html">Update log</a></li></ul>';
 
 document.getElementById("sidebar").innerHTML = html;
