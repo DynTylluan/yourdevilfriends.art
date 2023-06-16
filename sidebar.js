5c9beeeed2c01ee32be66fa9b71c5d8eeef90a3e
yourdevilfriends <heckmaster@protonmail.com>
2023-06-16 22:00:09 +0100

    Changes that are so minor.

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,6 @@
 var html = '<a href="/"><button style="background-image: url(/images/backgrounds/buttons/index.png);">Index</button></a>';
 
-html +='<button style="background-image: url(/images/backgrounds/buttons/personal.png);">Personal</button></summary><ul><li><a href="/about.html">About</a></li><li><a href="/blog.html">Inane rambling</a></li></ul>';
+html +='<button style="background-image: url(/images/backgrounds/buttons/personal.png);">Personal</button></summary><ul><li><a href="/about.html">About</a></li><li><a href="/blog.html">Blog</a></li><li><a href="/characters.html">Character hub</a></li></ul>';
 
 html +=
     '<button style="background-image: url(/images/backgrounds/buttons/artwork.png);">Artwork</button></summary><ul><li><a href="/gallery.html">Art gallery</a></li><li><a href="/comix.html">Comics</a></li></ul>';
