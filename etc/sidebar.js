daccc413ce33079c33ef658075a1d966405acdc2
devils <107942837+yourdevilfriends@users.noreply.github.com>
2023-09-15 08:10:58 +0000

    Redirects

--- etc/sidebar.js
+++
@@ -1,9 +0,0 @@
-var html = '<h3>Navigation</h3><div class="navlink"><a href="/">Index</a>';
-
-var section1 = ['webmaster', 'art', 'writing', 'fun', 'links']
-for (i = 0; i<section1.length; i++){
-	html += '<div class="navlink"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
-}
-
-
-document.getElementById("sidebar").innerHTML = html;
